602e5a412bce33e97ee9b8598372e900e0b00bfc
Himatheja <htposa@gmail.com>
2026-08-01 20:14:52 +0530

    Redirect root URL to login page

--- student_portal/urls.py
+++ student_portal/urls.py
@@ -1,3 +1,4 @@
+from django.views.generic import RedirectView
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -11,6 +12,7 @@ urlpatterns = [
     path('courses/', include('courses.urls')),
     path('expenses/', include('expenses.urls')),
     path('notes/', include('notes.urls')),
+path('', RedirectView.as_view(url='/accounts/login/', permanent=False)),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
